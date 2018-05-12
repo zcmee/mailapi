b56349a05b471876da773104da56efbde53ad906
Adam <zcm@interia.pl>
2018-05-12 19:43:57 +0200

    First commit

---
+++ src/main/java/com/github/zcmee/mailapi/MailapiApplication.java
@@ -0,0 +1,12 @@
+package com.github.zcmee.mailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailapiApplication.class, args);
+	}
+}
